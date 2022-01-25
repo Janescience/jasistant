b9773d47b7601227c5d9131f529772ed5fe9213b
Glitch (glitch-blank-node) <none>
2022-01-25 12:17:47 +0000

    Exporting to glitchdotcom/glitch-blank-node ./server.js:31317407/125

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ fastify.post("/", function(request, reply) {
 });
 
 // Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
+fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
