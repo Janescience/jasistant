912eaca4560d028660ee7d82dc21135f861f8613
Glitch (glitch-blank-node) <none>
2022-06-22 11:24:42 +0000

    Exporting to glitchdotcom/glitch-blank-node ./package.json:31317407/4459 ./server.js:31317407/3061 ./LICENSE:31317407/67

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require("path");
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
   // set this to true for detailed logging:
-  logger: false
+  logger: false,
 });
 
 // Setup our static files
 fastify.register(require("@fastify/static"), {
   root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
+  prefix: "/", // optional: default '/'
 });
 
 // fastify-formbody lets us parse incoming forms
@@ -18,35 +18,38 @@ fastify.register(require("@fastify/formbody"));
 // point-of-view is a templating manager for fastify
 fastify.register(require("@fastify/view"), {
   engine: {
-    handlebars: require("handlebars")
-  }
+    handlebars: require("handlebars"),
+  },
 });
 
 // Our main GET home page route, pulls from src/pages/index.hbs
-fastify.get("/", function(request, reply) {
+fastify.get("/", function (request, reply) {
   // params is an object we'll pass to our handlebars template
   let params = {
-    greeting: "Hello Node!"
+    greeting: "Hello Node!",
   };
   // request.query.paramName <-- a querystring example
   return reply.view("/src/pages/index.hbs", params);
 });
 
 // A POST route to handle form submissions
-fastify.post("/", function(request, reply) {
+fastify.post("/", function (request, reply) {
   let params = {
-    greeting: "Hello Form!"
+    greeting: "Hello Form!",
   };
   // request.body.paramName <-- a form post example
   return reply.view("/src/pages/index.hbs", params);
 });
 
 // Run the server and report out to the logs
-fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
   }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
-});
+);
