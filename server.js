13caa84dbbe801451ff8f274eb951b4bc3c67bf3
Glitch (glitch-blank-node) <none>
2022-12-06 12:08:53 +0000

    Exporting to glitchdotcom/glitch-blank-node ./server.js:31317407/74 ./package.json:31317407/3409

--- server.js
+++ server.js
@@ -46,10 +46,9 @@ fastify.listen(
   { port: process.env.PORT, host: "0.0.0.0" },
   function (err, address) {
     if (err) {
-      fastify.log.error(err);
+      console.error(err);
       process.exit(1);
     }
     console.log(`Your app is listening on ${address}`);
-    fastify.log.info(`server listening on ${address}`);
   }
 );
