064a4892221db608ebb39316d73e7a12a97ebbae
Glitch (glitch-blank-node) <none>
2023-05-11 07:56:57 +0000

    🏉⛰ Checkpoint ./src/GenerateQrcode.js:57913848/50

--- src/GenerateQrcode.js
+++ src/GenerateQrcode.js
@@ -6,11 +6,11 @@ const generateQrcode = async (amount) => {
   const mobileNumber = '080-608-1559'
   const payload = generatePayload(mobileNumber, { amount })
   const options = { type: 'svg', color: { dark: '#003b6a', light: '#f7f8f7' } }
-
-  await new Promise((resolve, reject) => {
-    return qrcode.toString(payload, options, (err, svg) => {
+  return payload;
+   await new Promise((resolve, reject) => {
+    qrcode.toString(payload, options, (err, svg) => {
       if (err) return reject(err)
-        resolve(svg)
+         resolve(svg)
     })
   })
   
