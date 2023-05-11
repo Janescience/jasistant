57546ebaecdde926899524d646c682015128c414
Glitch (glitch-blank-node) <none>
2023-05-11 10:15:14 +0000

    🎷🏹 Checkpoint ./src/GenerateQrcode.js:57913848/721 ./package.json:57913848/19

--- src/GenerateQrcode.js
+++ src/GenerateQrcode.js
@@ -1,30 +1,30 @@
 const qrcode = require('qrcode')
 const generatePayload = require('promptpay-qr')
-const svgToDataURL = require('svg-to-dataurl')
+const fs = require('fs') 
 const { createBubble } = require("./LineMessageUtility");
 
 const generateQrcode = async (amount) => {
 
   const mobileNumber = '080-608-1559'
   const payload = generatePayload(mobileNumber, { amount })
-  const options = { type: 'svg', color: { dark: '#003b6a', light: '#f7f8f7' } }
   
-  // const imageUrl = 'data:image/svg+xml,<svg>...</svg>';
-  // const previewUrl = 'data:image/png;base64,iVBORw0KG...';
+  const options = { type: 'svg', color: { dark: '#000', light: '#fff' } }
   
-  const qrSvg =  await qrcode.toString(payload, options)
-  const imageUrl = svgToDataURL(qrSvg);
-  
-  const previewUrl =  await qrcode.toDataURL(payload)
+  qrcode.toString(payload, options, (err, svg) => {
+      if (err) return console.log(err)
+      fs.writeFileSync('./qr.svg', svg)
+      console.log(svg)
+  })
 
-  const message = {
-    type: 'image',
-    originalContentUrl: encodeURIComponent(imageUrl),
-    previewImageUrl: encodeURIComponent(previewUrl)
-  };
+//   const message = {
+//     type: 'image',
+//     originalContentUrl: encodeURIComponent(imageUrl),
+//     previewImageUrl: encodeURIComponent(previewUrl)
+//   };
 
   // const urls = svgToDataURL(qr)
-  return message;
+  // return message;
+  return 
 }
 
 module.exports = generateQrcode;
