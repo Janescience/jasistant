6fb9f9e52b771cf377d6e1bd8b0603c52a6996d6
Glitch (glitch-blank-node) <none>
2023-05-11 09:41:04 +0000

    💠🍳 Checkpoint ./src/GenerateQrcode.js:57913848/1186

--- src/GenerateQrcode.js
+++ src/GenerateQrcode.js
@@ -8,14 +8,27 @@ const generateQrcode = async (amount) => {
   const mobileNumber = '080-608-1559'
   const payload = generatePayload(mobileNumber, { amount })
   const options = { type: 'svg', color: { dark: '#003b6a', light: '#f7f8f7' } }
+  
+  // const imageUrl = 'data:image/svg+xml,<svg>...</svg>';
+  const previewUrl = 'data:image/png;base64,iVBORw0KG...';
+  
+  const imageUrl =  await new Promise((resolve, reject) => {
+    qrcode.toString(payload, options, (err, svg) => {
+      if (err) return reject(err)
+         resolve(svg)
+    })
+  })
+  
+  const previewUrl =  await qrcode.toDataURL(payload)
 
-  // const qr =  await new Promise((resolve, reject) => {
-  //   qrcode.toString(payload, options, (err, svg) => {
-  //     if (err) return reject(err)
-  //        resolve(svg)
-  //   })
-  // })
-  const qr =  await qrcode.toDataURL(payload)
+
+  const message = {
+    type: 'image',
+    originalContentUrl: imageUrl,
+    previewImageUrl: previewUrl
+  };
+
+
   // const urls = svgToDataURL(qr)
   return {
     "type": "image",
