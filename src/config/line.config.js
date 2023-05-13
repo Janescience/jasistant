dc46c86295220135aff9332310f0fdbb8f3244f4
Glitch (glitch-blank-node) <none>
2023-05-13 15:46:24 +0700

    fix line.config

--- src/config/line.config.js
+++ src/config/line.config.js
@@ -1,4 +1,8 @@
-module.exports = {
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRET
+const config = () => {
+    return { 
+        channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+        channelSecret: process.env.LINE_CHANNEL_SECRET
+    }
 }
+
+module.exports = config
