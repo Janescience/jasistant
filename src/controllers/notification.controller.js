d81df3f39541ac962321a9d6581f4073c4809bf5
Glitch (glitch-blank-node) <none>
2023-05-25 16:40:16 +0700

    update key to .env

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -1,11 +1,10 @@
 const sealedbox = require('tweetnacl-sealedbox-js');
-const keys = require('./keys.json')
 
 exports.notification = async (req, res) => {
     const result = sealedbox.open(
       Buffer.from(req.body, 'hex'),
-      Buffer.from(keys.publicKey, 'base64'),
-      Buffer.from(keys.secretKey, 'base64')
+      Buffer.from(process.env.NOTI_PUBLIC_KEY, 'base64'),
+      Buffer.from(process.env.NOTI_SECRET_KEY, 'base64')
     );
     
     console.log(Buffer.from(result).toString());
