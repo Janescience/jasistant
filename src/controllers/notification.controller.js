0e6477b398e42b1992aabf41ff6d0f06a25f5210
janescience <janescience.top@gmail.com>
2023-06-07 11:43:16 +0700

    update code in notification controller

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -1,10 +1,14 @@
 const sealedbox = require('tweetnacl-sealedbox-js');
 const expenseTracking = require("../modules/expense-tracking.module")
-const { Client } =  require('@line/bot-sdk')
 const config = require("../config/line.config");
+
+const { Client } =  require('@line/bot-sdk')
 const { toMessages } = require('../utilities/line.utility')
 
 exports.notification = async (req, res) => {
+
+  const client = new Client(config())
+
   const result = sealedbox.open(
     Buffer.from(req.body, 'hex'),
     Buffer.from(process.env.NOTI_PUBLIC_KEY, 'base64'),
@@ -18,19 +22,13 @@ exports.notification = async (req, res) => {
   const text = notification?.text
   const time = notification?.time
 
-  console.log('notification package : ',packageName)
-  console.log('notification title : ',title)
-
   const amount = await text.match(/\d+(\.\d+)/g).map(function(v) { return parseFloat(v); });
-  console.log('notification amount : ',amount)
 
   if(packageName == "com.kasikorn.retail.mbanking.wap"){
     if(title == 'รายการโอน/ถอน' || title == 'รายการใช้บัตร'){
-      console.log('expense recording...')
       const recorded = await expenseTracking(new Date(time),title,amount[0], "transfer")
-      const client = new Client(config())
       await client.pushMessage(process.env.LINE_USER_ID,toMessages(recorded.message))
-      return res.status(200).send({message : 'Expense recording success.'})
+      return res.status(200).send({message : 'Expense record successfully.'})
     }
   }
 };
