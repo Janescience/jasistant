8bcc139426456cc95a89723da0a35519d3da0cf1
Glitch (glitch-blank-node) <none>
2023-09-18 13:10:09 +0700

    change regex get currency from string in notification

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -1,12 +1,7 @@
 const sealedbox = require('tweetnacl-sealedbox-js');
 const expenseTracking = require("../modules/expense-tracking.module")
-const config = require("../config/line.config");
-
-const { Client } =  require('@line/bot-sdk')
 
 exports.notification = async (req, res) => {
-
-  const client = new Client(config())
 
   const result = sealedbox.open(
     Buffer.from(req.body, 'hex'),
@@ -21,12 +16,21 @@ exports.notification = async (req, res) => {
   const text = notification?.text
   const time = notification?.time
 
-  const amount = await text.match(/\d+(\.\d+)/g).map(function(v) { return parseFloat(v); });
 
-  if(packageName == "com.kasikorn.retail.mbanking.wap"){
-    if(title == 'รายการโอน/ถอน' || title == 'รายการใช้บัตร'){
-      await expenseTracking(new Date(time),title,amount[0], "transfer")
-      return res.status(200).send({message : 'Expense recording success.'})
+  let amount = 0;
+  let match;
+
+  // Regex get Currency from String
+  if(match = text.match(/(?!0\.00)\d{1,3}(,\d{1,9})*(\.\d\d)+(?:\.)?/g)){
+    amount = Number(match[0].replace(/[^0-9.-]+/g,""))
+  }
+   
+  if(amount > 0){
+    if(packageName == "com.kasikorn.retail.mbanking.wap"){
+      if(title == 'รายการโอน/ถอน' || title == 'รายการใช้บัตร'){
+        await expenseTracking(new Date(time),title,amount, "transfer")
+        return res.status(200).send({message : 'Expense recording success.'})
+      }
     }
   }
 };
